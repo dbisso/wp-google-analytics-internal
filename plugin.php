51ca20efdf697a67f83fe494b173475395db7d49
Dan Bissonnet <dan@danisadesigner.com>
2014-02-26 18:11:34 +0000

    Create an array of WP events => GA action text. Allow it to be filtered and use it in the post_published hook.

--- plugin.php
+++ plugin.php
@@ -57,9 +57,39 @@ class DBisso_GoogleAnalyticsInternal {
 	 * @param  int $post_id The post ID.
 	 */
 	static public function action_publish_post( $post_id ) {
-		$event = new DBisso_GoogleAnalyticsInternal_Event( __( 'Publish Post', 'dbisso-google-analytics-internal' ), get_the_title( (int) $post_id ) );
+		$separate_update_events = self::get_option( 'separate_update_events' );
+
+		$event = new DBisso_GoogleAnalyticsInternal_Event(
+			self::get_event_action( 'publish_post' ),
+			get_the_title( (int) $post_id )
+		);
+
 		$event->send();
 	}
+	/**
+	 * Get the action string for a given WP event
+	 * @param  string $hook The name of the hook.
+	 * @return string       The action for the hook.
+	 */
+	static private function get_event_action( $hook ) {
+		return self::get_event_action( $hook );
+	}
+
+	/**
+	 * Returns the filtered string to use as the action in the GA event.
+	 * @return array Array with WP event => action text.
+	 */
+	static private function get_event_actions() {
+		$actions = array(
+			'publish_post' => __( 'Publish Post', 'dbisso-google-analytics-internal' ),
+			'update_post' => __( 'Update Post', 'dbisso-google-analytics-internal' ),
+			'comment_post' => __( 'Comment Submitted', 'dbisso-google-analytics-internal' ),
+			'comment_approved' => __( 'Comment Approved', 'dbisso-google-analytics-internal' )
+		);
+
+		return apply_filters( 'dbisso_gai_event_actions', $actions );
+	}
+
 }
 
 // Start the plugin.
