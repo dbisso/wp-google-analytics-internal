e5d2f907e657344050961f561be16c3db7d05315
Dan Bissonnet <dan@danisadesigner.com>
2014-02-26 18:13:48 +0000

    Introduce helper function to test whether a post is already published

--- plugin.php
+++ plugin.php
@@ -105,6 +105,9 @@ class DBisso_GoogleAnalyticsInternal {
 		return apply_filters( 'dbisso_gai_event_actions', $actions );
 	}
 
+	static private function is_post_published( $post_id ) {
+		return get_post_status( $post_id ) === 'publish';
+	}
 }
 
 // Start the plugin.
