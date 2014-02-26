43be4f99a6f259b679d46f3030051bc30fc53440
Dan Bissonnet <dan@danisadesigner.com>
2014-02-26 17:36:17 +0000

    Use Event class to manage the dispatch of events.
    Also: also allow translation of strings

--- plugin.php
+++ plugin.php
@@ -3,9 +3,10 @@
 Plugin Name: Google Analytics Internal
 Plugin URI: http://danisadesigner.com/plugins/google-analytics-internal
 Description: Use Google Analytics events to track when you publish posts.
-Version: 0.1.0
+Version: 0.2.0
 Author: Dan Bissonnet
 Author URI: http://danisadesigner.com/
+Text Domain: dbisso-google-analytics-internal
 */
 
 /**
@@ -30,18 +31,25 @@ Author URI: http://danisadesigner.com/
  * **********************************************************************
  */
 
+include_once 'lib/DBisso_GoogleAnalyticsInternal_Event.php';
 /**
  * Triggers Google Analytics measurement when internal WordPress events
  * occur.
  */
 class DBisso_GoogleAnalyticsInternal {
-	static $ga_endpoint = 'https://ssl.google-analytics.com/collect';
-
 	/**
 	 * Set up and bind hooks.
 	 */
 	static public function bootstrap() {
-		add_action( 'publish_post', array( __CLASS__, 'action_publish_post' ), 10, 99 );
+		add_action( 'publish_post', array( __CLASS__, 'action_publish_post' ), 10, 1 );
+		add_action( 'plugins_loaded', array( __CLASS__, 'plugins_loaded' ) );
+	}
+
+	/**
+	 * Load our text domain
+	 */
+	function plugins_loaded() {
+		load_plugin_textdomain( 'dbisso-google-analytics-internal', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
@@ -49,83 +57,8 @@ class DBisso_GoogleAnalyticsInternal {
 	 * @param  int $post_id The post ID.
 	 */
 	static public function action_publish_post( $post_id ) {
-		$post_args = self::get_post_data_event( 'Publish Post', get_the_title( (int) $post_id ) );
-
-		// No UA string found so bail out
-		if ( empty( $post_args['tid'] ) ) {
-			return;
-		}
-
-		$result = wp_remote_post(
-			self::$ga_endpoint,
-			array( 'body' => $post_args )
-		);
-	}
-
-	/**
-	 * Get POST data for triggering an event
-	 * @param  string  $action The event action.
-	 * @param  string $label   The event label.
-	 * @param  int $value      Value to assign to the event.
-	 * @return array           The data to send with the POST request
-	 */
-	private static function get_post_data_event( $action, $label = false, $value = false ) {
-		$data = self::get_post_data();
-
-		$data['t']  = 'event';
-		$data['ea'] = $action;
-
-		if ( $label ) {
-			$data['el'] = $label;
-		}
-
-		if ( $value ) {
-			$data['el'] = $label;
-		}
-
-		return $data;
-	}
-
-	/**
-	 * Get the default POST data for a measurement protocol request.
-	 * @return array The data for the POST request
-	 */
-	private static function get_post_data() {
-		$user = wp_get_current_user();
-		$ua   = self::get_analytics_ua();
-
-		$data = array(
-			'v' => 1,
-			'tid' => $ua,
-			'cid' => $user->id,
-			't' => 'event',
-			'ec' => 'WordPress',
-		);
-
-		return $data;
-	}
-
-	/**
-	 * Get the Google Analytics UA string.
-	 *
-	 * Use string from Yoast's Google Analytics is installed. String
-	 * can be overridden with a global constant.
-	 *
-	 * @return string|boolean The UA string to use or false if none found.
-	 */
-	private static function get_analytics_ua() {
-		$yoast_config = get_option( 'Yoast_Google_Analytics' );
-		$ua           = false;
-
-		if ( $yoast_config && ! empty( $yoast_config['uastring'] ) ) {
-			$ua = $yoast_config['uastring'];
-		}
-
-		if ( defined( 'DBISSO_GA_UA' )  ) {
-			$ua = DBISSO_GA_UA;
-		}
-
-		return $ua;
+		$event = new DBisso_GoogleAnalyticsInternal_Event( __( 'Publish Post', 'dbisso-google-analytics-internal' ), get_the_title( (int) $post_id ) );
+		$event->send();
 	}
 }
 
