9ddc0c7238082cd495a1ea22633c6803e0e48fa2
Dan Bissonnet <dan@danisadesigner.com>
2015-04-10 15:20:45 +0100

    Note loose comparison

--- lib/DBisso_GoogleAnalyticsInternal.php
+++ lib/DBisso_GoogleAnalyticsInternal.php
@@ -113,6 +113,7 @@ class DBisso_GoogleAnalyticsInternal {
 	static public function action_wp_insert_comment( $comment_id, $comment ) {
 		$status = null;
 
+		// Loose comparison so we compare with strings instead of ints
 		switch ( $comment->comment_approved ) {
 		 	case '0':
 		 		$status = 'unapproved';
